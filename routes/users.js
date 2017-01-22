1aa1f98b57c3cb611ec7832cfc571ddb2832bfde
Sed Cash <Sed Cash>
2017-01-22 06:07:11 -0500

    Fourth Commit

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '0f548ed7dd8c4dc5a6c39562de4135d7'; // Your client id
 var client_secret = 'fc345574f2dc4c8aa96a4eba32ca86d7'; // Your secret
-var redirect_uri = 'http://sedcash.herokuapp.com/users/'; // Your redirect uri
+var redirect_uri = 'http://sedcash.herokuapp.com/users/callback'; // Your redirect uri
 
 var router = express.Router();
 
@@ -21,58 +21,51 @@ router.get('/callback', function(req, res) {
     // after checking the state parameter
 
     var code = req.query.code || null;
-    var state = req.query.state || null;
 
-    if (state === null || state !== storedState) {
-        res.redirect('/#' +
-            querystring.stringify({
-                error: 'state_mismatch'
-            }));
-    } else {
-        var authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: redirect_uri,
-                grant_type: 'authorization_code'
-            },
-            headers: {
-                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-            },
-            json: true
-        };
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code: code,
+            redirect_uri: redirect_uri,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+    };
 
-        request.post(authOptions, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
 
-                var access_token = body.access_token,
-                    refresh_token = body.refresh_token;
+            var access_token = body.access_token,
+                refresh_token = body.refresh_token;
 
-                var options = {
-                    url: 'https://api.spotify.com/v1/me',
-                    headers: { 'Authorization': 'Bearer ' + access_token },
-                    json: true
-                };
+            var options = {
+                url: 'https://api.spotify.com/v1/me',
+                headers: { 'Authorization': 'Bearer ' + access_token },
+                json: true
+            };
 
-                // use the access token to access the Spotify Web API
-                request.get(options, function(error, response, body) {
-                    console.log(body);
-                });
+            // use the access token to access the Spotify Web API
+            request.get(options, function(error, response, body) {
+                console.log(body);
+            });
 
-                // we can also pass the token to the browser to make requests from there
-                res.redirect('/#' +
-                    querystring.stringify({
-                        access_token: access_token,
-                        refresh_token: refresh_token
-                    }));
-            } else {
-                res.redirect('/#' +
-                    querystring.stringify({
-                        error: 'invalid_token'
-                    }));
-            }
-        });
-    }
+            // we can also pass the token to the browser to make requests from there
+            res.redirect('/#' +
+                querystring.stringify({
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                }));
+        } else {
+            res.redirect('/#' +
+                querystring.stringify({
+                    error: 'invalid_token'
+                }));
+        }
+    });
+
 });
 
 module.exports = router;
